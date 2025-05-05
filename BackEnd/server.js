416f1f4ef6cb3e96784dba7ec11f49e0c8528470
AJe-T <tejateju205101@gmail.com>
2025-05-06 00:13:37 +0530

    feat: Implement message sending functionality and protect routes with middleware

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,22 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messagesRoutes from "./routes/messages.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messagesRoutes);
-
+app.use("/api/message", messagesRoutes);
 
 app.listen(5000, () => {
   connectToMongoDB();
